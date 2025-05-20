900973ea1b6ff0f577360e4c2128a308c4de54e1
samuel asare <samuelaboahasare@gmail.com>
2025-05-20 21:49:44 +0000

    task: calculates the simple interest earned on an investment over a period of time.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,19 @@
+
+"""
+calculates the simple interest earned on an investment over a period of time.
+The formula for simple interest is (I = P * R * T)
+
+( I ) is the interest earned,
+( P ) is the principal amount (initial investment),
+( R ) is the annual interest rate (as a decimal),
+( T ) is the time the money is invested for in years.
+"""
+
+principal = 1000
+rate =  0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
+
