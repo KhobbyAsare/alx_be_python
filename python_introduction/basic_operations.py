03ff77fe33bf665df048a1d7c3452521a0771a1b
samuel asare <samuelaboahasare@gmail.com>
2025-05-20 21:44:13 +0000

    completed work commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+# Calculating the summation
+
+print(number1 + number2)
+
+
+# Calculating the difference (subtraction)
+
+print(number1 - number2)
+
+# Calculating the product
+
+print(number1 * number2)
