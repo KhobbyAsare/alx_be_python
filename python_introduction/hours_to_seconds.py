e62e32de38b3bae0c9c4e3c5279b413d56042762
samuel asare <samuelaboahasare@gmail.com>
2025-05-20 21:59:56 +0000

    fix:checker want it this way seconds = hours * 3600 instead of seconds_in_hour = 3600 seconds = hours * seconds_in_hour

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -6,7 +6,8 @@ Remember, there are 3600 seconds in an hour
 (since there are 60 minutes in an hour and 60 seconds in a minute, thus 60 x 60 = 3600).
 """
 hours = 2
-seconds_in_hour = 3600
+# seconds_in_hour = 3600
+
 seconds = hours * 3600
 
 print(f"{hours} hour(s) is {seconds} seconds.")
