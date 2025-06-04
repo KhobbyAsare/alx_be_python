648b7e4325bcf4b12abf32b2c376d54e8c15468f
samuel asare <samuelaboahasare@gmail.com>
2025-06-04 19:57:42 +0000

    task: define a function that performs basic arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -20,7 +20,8 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num1 > num2:
-          return num1 / num2
-        else:
-         return num2 / num1
+        if num2 > 0:
+            if num1 > num2:
+                return num1 / num2
+            else:
+                return num2 / num1
