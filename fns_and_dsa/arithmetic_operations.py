a0179fc03783968ea590beacdd6d2208bce9a8a1
samuel asare <samuelaboahasare@gmail.com>
2025-06-04 20:26:53 +0000

    task: define a function that performs basic arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,4 @@
 def perform_operation(num1, num2, operation):
-    # result = 0
-    # match operation:
-    #     case "add":
-    #         result = num1 + num2
-    #     case "subtraction":
-    #         result = num1 - num2
-    #     case "multiply":
-    #         result = num1 * num2
-    #     case "divide":
-    #         if num1 > num2:
-    #             result = num1 / num2
-    #         else:
-    #             result = num2 / num1
-    # return result
     if operation == "add":
         return num1 + num2
     elif operation == "subtraction":
@@ -20,8 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 > 0:
-            if num1 > num2:
-                return num1 / num2
-            else:
-                return num2 / num1
+        if num1 == 0 or num2 == 0:
+            return "Error: Division by zero is not allowed."
+        if num1 > num2:
+            return num1 / num2
+        else:
+            return num2 / num1
